01a2cbddabb96499b22998bc5b7406b74a7cb2f3
Edgar Figueroa <edgarefigueroa@outlook.com>
2022-10-10 17:36:08 -0400

    path

--- server.js
+++ server.js
@@ -3,7 +3,20 @@ const app = express()
 
 app.use(express.json())
 
+// endpoint for index.html
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'))
+    rollbar.info('Welcome to Duel Duo')
+})
 
+// endpoint for index.js 
+app.get("/js", (req, res) => {
+    res.sendFile(path.join(__dirname, '/script.js'))
+    
+  })
+
+// middleware 
+app.use("/styles", express.static(path.join(__dirname, "./style.css")));
 
 
 const port = process.env.PORT || 3000
