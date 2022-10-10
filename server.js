efb04ea06100300a11105a722c044fae20f3c7da
Edgar Figueroa <edgarefigueroa@outlook.com>
2022-10-10 17:01:00 -0400

    server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+
+
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
